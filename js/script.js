fbd5d5244ae1dc185bf76468f94ec561065d23a2
Paolo Alexander Marroquin <alexandermarroquinjunior@gmail.com>
2026-07-09 15:03:32 -0600

    CV Paolo Alexander Marroquin - Actualizado y optimizado

---
+++ js/script.js
@@ -0,0 +1,5 @@
+document.getElementById('year').textContent=new Date().getFullYear();
+
+document.getElementById('temaBtn').addEventListener('click',()=>{
+document.body.classList.toggle('oscuro');
+});
